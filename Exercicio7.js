6f27d167df77a4ba2ccacff5669999ec63df1369
momatos20set <dev.matos.one@gmail.com>
2021-10-05 20:22:15 -0300

    exercicio4

---
+++ Exercicio7.js
@@ -0,0 +1,8 @@
+
+var name = A
+var name = B
+var name = C
+var resultado 
+
+resultado if ( ( A<B+C e B<A+C e C<A+B ) 
+else resultado = "Lados não formam um triângulo" SeNão Se ( A = B = C ) então resultado "É um triângulo equilátero" SeNão Se ( ( A=B e B≠C ) ou ( A=C e C≠B ) ou ( B=C e C≠A ) ) então resultado = "É um triângulo isósceles" SeNão Se ( ( A ≠ B ) e ( B ≠ C ) e ( A ≠ C ) ) então resultado = "É um triângulo escaleno" FimSe FimSe FimSe FimSe Imprimi resultado Fim
---
+++ exercicio04.js
@@ -0,0 +1,30 @@
+var salario;
+var salarioNovo;
+var aumento;
+
+salario = input(("Digite o Salário do Colaborador: ") + ".");
+
+    if (salario <= 2800) {
+        aumento = (salario * 20 / 100);
+        salarioNovo = aumento + salario;
+        console.log("Salário Antes do Reajuste: R$ " + salario + ".\n-- Aumento Aplicado 20% ----\nValor do Aumento: R$ " + aumento + ".\nSalário Após o Aumento: R$ " + salarioNovo + ".");
+    }
+
+    else if (salario > 2800 && salario <= 7000) {
+        aumento = (salario * 15 / 100);
+        salarioNovo = aumento + salario;
+        console.log("Salário Antes do Reajuste: R$ " + salario + ".\n-- Aumento Aplicado 15% --\nValor do Aumento: R$ " + aumento + ".\nSalário Após o Aumento: R$ " + salarioNovo + ".");
+    }
+
+    else if (salario > 7000 && salario <= 15000) {
+        aumento = (salario * 10 / 100);
+        salarioNovo = aumento + salario;
+        console.log("Salário Antes do Reajuste: R$ " + salario + ".\n-- Aumento Aplicado 10% --\nValor do Aumento: R$ " + aumento + ".\nSalário Após o Aumento: R$ " + salarioNovo + ".");
+    }
+
+    else {
+        aumento = (salario * 5 / 100);
+        salarioNovo = aumento + salario;
+        console.log("Salário Antes do Reajuste: R$ " + salario + ".\n-- Aumento Aplicado 5% --\nValor do Aumento: R$ " + aumento + ".\nSalário Após o Aumento: R$ " + salarioNovo + ".");
+    };
+
