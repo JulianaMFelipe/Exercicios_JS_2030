98fed1228fae26c3e9b40f4f280be0e2dd55b2df
momatos20set <dev.matos.one@gmail.com>
2021-10-05 21:05:51 -0300

    Exercicio05

---
+++ exercicio05.js
@@ -0,0 +1,25 @@
+num = 8;
+
+if (num == 1){
+    console.log("Domingo");
+}
+else if (num == 2){
+    console.log("Segunda-feira");
+}
+else if (num == 3){
+    console.log("terça-feira");
+}
+else if (num == 4){
+    console.log("quarta-feira");
+}
+else if (num == 5){
+    console.log("quinta-feira");
+}
+else if (num == 6){
+    console.log("sexta-feira");
+}
+else if (num == 7){
+    console.log("Sabado");
+}
+else 
+    console.log("Valor inválido!");
